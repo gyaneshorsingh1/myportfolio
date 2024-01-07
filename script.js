187b53c3159a6a2227c7e534e7daedba2195e6cc
gyaneshorsingh1 <gyaneshorsingh9@gmail.com>
2024-01-08 01:43:14 +0530

    contact db set

--- script.js
+++ script.js
@@ -10,4 +10,16 @@ function showSidebar(){
 }
 
 
+// const form = document.querySelector("form");
+// form.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     let data = new FormData(form);
+//     fetch('https://script.google.com/macros/s/AKfycbw49_3WZwXyJasjIBoOBTODComm5U_0P5w38dGbWMZGICtAIJOhvXNXiFDnXgfOiank/exec'{
+//         method:"POST",
+//         body: data
+//     })
+//     .then(res => res.text());
+//     .then(data=> console.log(data));
+// })
+
 
