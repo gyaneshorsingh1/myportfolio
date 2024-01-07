95985d3616c21f53cea0cc9abe04058bce98ec8c
gyaneshorsingh1 <gyaneshorsingh9@gmail.com>
2024-01-08 04:03:33 +0530

    gsap animation added to content or main part

--- script.js
+++ script.js
@@ -24,3 +24,41 @@ document.getElementById("tempCon").addEventListener("click",function(){
 
 
 
+
+//animation for all the content of the body//
+var tl= gsap.timeline()
+
+tl.from(".nav, .nav span, .nav .page,.social-icons",{
+    y: -100,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:0.3
+})
+tl.from(".social-icons span",{
+    x: 300,
+    duration:0.5,
+    delay:0.1,
+    opacity:0,
+    stagger:0.2
+})
+tl.from(".content h2,.content button",{
+    x: 300,
+    duration:1,
+    opacity:0,
+    
+})
+tl.from(".content p",{
+    x: -300,
+    duration:2,
+    opacity:0,
+    
+})
+// tl.from(".content button",{
+//     y:200,
+//     opacity:0,
+//     delay:0.01
+// })
+
+
+
